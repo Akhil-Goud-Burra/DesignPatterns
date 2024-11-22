77c4b90fbde35f44aa97e7a52df4d0701a151a63
Akhil Goud Burra <124227421+Akhil-Goud-Burra@users.noreply.github.com>
2024-11-22 00:21:24 -0600

    Add files via upload

---
+++ Design Patterns/The Adapter Pattern/Code/MallardDuck.java
@@ -0,0 +1,14 @@
+import Fly.FlyWithWings;
+import Quack.Quack;
+
+public class MallardDuck extends Duck {
+    public MallardDuck() {
+        quackBehavior = new Quack();
+        flyBehavior = new FlyWithWings();
+    }
+
+    @Override
+    void display() {
+        System.out.println("I'm a real Mallard duck!");
+    }
+}
